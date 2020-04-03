30913eaa4466f3c4d8ce9583075aca05a5dee63d
Nevena Cukucan <ljubicpantic@gmail.com>
2020-04-03 19:36:45 -0400

    added two printing methods

--- functions.py
+++ functions.py
@@ -1,11 +1,12 @@
+
 def get_names():
     answer_list = []
 
     for index in range(5):
-        answer = input("List any 5 of the first 20 elements: " + str(index+1) + "# - ")
+        answer = input("List any 5 of the first 20 elements: " + str(index + 1) + "# - ")
         while answer.lower() in answer_list or answer == "":
             print("An entry cannot be a duplicate or an empty string. Try again.")
-            answer = input("Enter a unique element: " + str(index+1) + "# - ")
+            answer = input("Enter a unique element: " + str(index + 1) + "# - ")
         answer_list.append(answer.lower())
 
     return answer_list
@@ -17,28 +18,34 @@ def calculate_score(list):
 
 
 def print_results(points, correct, incorrect):
-    separator = "******************************************************************"
-    string = ""
-    print(separator)
-    print("You've completed the quiz!")
-    print(separator)
+    print_banner("You've completed the quiz!")
     if points >= 80:
-        print("Well done! You seem to know elements well! Your score is "+ str(points) + "%.")
+        print_separator("Well done! You seem to know elements well! Your score is " + str(points) + "%.")
     elif 20 < points < 80:
-        print("You might want to refresh your knowledge of elements. You score is " + str(points) +"%.")
+        print_separator("You might want to refresh your knowledge of elements. You score is " + str(points) + "%.")
     else:
         print("You should go back to school - your score is " + str(points) + "%.")
-    print(separator)
 
     if len(correct) > 0:
         string = ", ".join(correct)
-        print("Correct answers: ", string.title())
+        print_separator("Correct answers: " + string.title())
     else:
-        print("Correct answers: none :(")
+        print_separator("Correct answers: none :(")
 
     if len(incorrect) > 0:
         string = ", ".join(incorrect)
-        print("Incorrect answers: ", string.title())
+        print_separator("Incorrect answers: " + string.title())
     else:
-        print("Incorrect answers: none - :)")
-    print(separator)
+        print_separator("Incorrect answers: none - :)")
+
+
+def print_banner(sentence):
+    line = "*" * len(sentence)
+    print(line)
+    print_separator(sentence)
+
+
+def print_separator(sentence):
+    line = "*" * len(sentence)
+    print(sentence)
+    print(line)
--- program_flow.py
+++ program_flow.py
@@ -1,45 +1,48 @@
-from functions import get_names, calculate_score, print_results
-
-#read the file with elements, strip unnecessary pieces of strings, close the file
-file = open("elements.txt", "r")
-element_list = file.readlines()
-
-for index in range(len(element_list)):
-    element_list[index] = element_list[index].strip("\n").lower()
-
-file.close()
+from functions import get_names, calculate_score, print_results, print_banner
 
 #print the instructions for a user
-print("Greetings! It is time to check your knowledge of ATOMIC ELEMENTS.")
-print("Enter any 5 of the first 20 atomic elements from Period Table and I will give you a grade.")
-
-user_input = input("Are you ready (y/n)?\n")
-print("******************************************************************")
-
-if user_input.lower() == "y" or user_input.lower() == "yes":
-    # get user input collected in a list
-    answer_list = get_names()
-
-    # distribute correct and incorrect answers in 2 separate lists
-    correct_answers = []
-    incorrect_answers = []
-
-    for answer in answer_list:
-        if answer in element_list:
-            correct_answers.append(answer)
-        else:
-            incorrect_answers.append(answer)
-
-    # calculate points
-    points = calculate_score(correct_answers)
-
-    # print results
-    print_results(points, correct_answers, incorrect_answers)
-else:
-    print("Sorry to see you go :(")
 
 
+def main():
+    # read the file with elements, strip unnecessary pieces of strings, close the file
+    file = open("elements.txt", "r")
+    element_list = file.readlines()
 
+    for index in range(len(element_list)):
+        element_list[index] = element_list[index].strip("\n").lower()
+
+    file.close()
+
+    print("Greetings! It is time to check your knowledge of ATOMIC ELEMENTS.")
+    print("Enter any 5 of the first 20 atomic elements from Period Table and I will give you a grade.")
+
+    user_input = input("Are you ready (y/n)?\n")
+
+    if user_input.lower() == "y" or user_input.lower() == "yes":
+        # get user input collected in a list
+        answer_list = get_names()
+
+        # distribute correct and incorrect answers in 2 separate lists
+        correct_answers = []
+        incorrect_answers = []
+
+        for answer in answer_list:
+            if answer in element_list:
+                correct_answers.append(answer)
+            else:
+                incorrect_answers.append(answer)
+
+        # calculate points
+        points = calculate_score(correct_answers)
+
+        # print results
+        print_results(points, correct_answers, incorrect_answers)
+    else:
+        print("Sorry to see you go :(")
+
+
+if __name__ == "__main__":
+    main()
 
 
 
